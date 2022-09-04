dd7de52573e8cafe0e7beabe2a4ad77fe8d9337a
Andrey Hrusheuski <gruand13@gmail.com>
2022-09-04 10:13:57 +0300

    made first simple methods

---
+++ src/js/lib/core.js
@@ -0,0 +1,47 @@
+// (()=>{
+//     const $ = function (selector){
+//         const elements = document.querySelectorAll(selector);
+//         const obj ={};
+        
+
+//         obj.hide = ()=>{
+//             elements.forEach(elem =>{
+//                 elem.style.display = 'none';
+//             });
+//             return obj;
+//         };
+
+//         obj.show = ()=>{
+//             elements.forEach(elem =>{
+//                 elem.style.display = '';
+//             });
+//             return obj;
+//         };
+
+//         return obj;
+//     };
+//     window.$ = $;
+// })();
+
+const $ = function(selector){
+    return new $.prototype.init(selector);
+};
+
+$.prototype.init = function(selector){
+    if(!selector){
+        return this; // {} вернет пустой обьект
+    }
+    // в существующий обьект добавляем новые свойства
+    Object.assign(this, document.querySelectorAll(selector));
+    this.length = document.querySelectorAll(selector).length;
+
+    return this;
+};
+
+// в прототип того обьекта который возвращает функция init мы записываем прототип нашей главной функции
+// теперь на обьекте который будет создаваться при помощи функции $ мы можем использовать любые методы которые будут внутри прототипа этой функции
+$.prototype.init.prototype = $.prototype;
+// записываем чтобы могли глобально использовать
+window.$ = $;
+//экспортируем чтобы использовать в других файлах
+export default $;
---
+++ src/js/lib/lib.js
@@ -0,0 +1,6 @@
+// этот файл нужен чтобы обогатить функцию $ различными методами
+
+import $ from './core';
+import './modules/display';
+
+export default $;

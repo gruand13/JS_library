054087e932cc1a67c0b91bf7653a63fcba06e1cb
Andrey Hrusheuski <gruand13@gmail.com>
2022-09-06 20:43:50 +0300

    made method for work with elements part2

--- dist/script.js
+++ dist/script.js
@@ -251,6 +251,53 @@ _core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.find = function (selecto
   return this;
 };
 
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.closest = function (selector) {
+  let counter = 0;
+
+  for (let i = 0; i < this.length; i++) {
+    this[i] = this[i].closest(selector);
+    counter++;
+  }
+
+  const objLength = Object.keys(this).length;
+
+  for (; counter < objLength; counter++) {
+    delete this[counter];
+  }
+
+  return this;
+};
+
+_core__WEBPACK_IMPORTED_MODULE_0__["default"].prototype.siblings = function () {
+  let numberOfItems = 0;
+  let counter = 0;
+  const copyObj = Object.assign({}, this);
+
+  for (let i = 0; i < copyObj.length; i++) {
+    const arr = copyObj[i].parentNode.children;
+
+    for (let j = 0; j < arr.length; j++) {
+      if (copyObj[i] === arr[j]) {
+        continue;
+      }
+
+      this[counter] = arr[j];
+      counter++;
+    }
+
+    numberOfItems += arr.length - 1;
+  }
+
+  this.length = numberOfItems;
+  const objLength = Object.keys(this).length;
+
+  for (; numberOfItems < objLength; numberOfItems++) {
+    delete this[numberOfItems];
+  }
+
+  return this;
+};
+
 /***/ }),
 
 /***/ "./src/js/lib/modules/attribute.js":
@@ -445,8 +492,10 @@ $('button').on('click', function () {
 });
 $('div').click(function () {
   console.log($(this).index());
-});
-console.log($('div').eq(2).find('.some')); // console.log($('button').html('hello '));
+}); // console.log($('div').eq(2).find('.some'));
+// console.log($('.some').closest('.findme'));
+
+console.log($('.findme').siblings()); // console.log($('button').html('hello '));
 
 /***/ })
 
--- src/js/main.js
+++ src/js/main.js
@@ -9,7 +9,12 @@ $('div').click(function(){
     console.log($(this).index());
 });
 
-console.log($('div').eq(2).find('.some'));
+// console.log($('div').eq(2).find('.some'));
+
+// console.log($('.some').closest('.findme'));
+
+
+console.log($('.findme').siblings());
 
 
 // console.log($('button').html('hello '));
